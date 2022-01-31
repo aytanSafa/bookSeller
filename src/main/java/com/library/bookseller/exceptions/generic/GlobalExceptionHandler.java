20ffa73d907eb9df565d4d48406b5da5ce048d3a
safaa <aytansafa@gmail.com>
2022-01-31 14:02:09 +0300

    Exception Handlers inserted

--- src/main/java/com/library/bookseller/exceptions/generic/GlobalExceptionHandler.java
+++ src/main/java/com/library/bookseller/exceptions/generic/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.library.bookseller.exceptions.generic;
 
-import com.library.bookseller.exceptions.AuthorServiceException;
+import com.library.bookseller.exceptions.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,6 +16,27 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponseModel> genericAuthorServiceException(AuthorServiceException authorServiceException){
         return new ResponseEntity<>(toExceptionModel(authorServiceException),authorServiceException.getHttpStatus());
     }
+
+    @ExceptionHandler(value = UserServiceException.class)
+    public ResponseEntity<ErrorResponseModel> genericUsersServiceException(UserServiceException userServiceException){
+        return new ResponseEntity<>(toExceptionModel(userServiceException),userServiceException.getHttpStatus());
+    }
+
+    @ExceptionHandler(value = AccountServiceException.class)
+    public ResponseEntity<ErrorResponseModel> genericAccountServiceException(AccountServiceException accountServiceException){
+        return new ResponseEntity<>(toExceptionModel(accountServiceException),accountServiceException.getHttpStatus());
+    }
+
+    @ExceptionHandler(value = CategoriesServiceException.class)
+    public ResponseEntity<ErrorResponseModel> genericCategoriesServiceException(CategoriesServiceException categoriesServiceException){
+        return new ResponseEntity<>(toExceptionModel(categoriesServiceException),categoriesServiceException.getHttpStatus());
+    }
+    @ExceptionHandler(value = BookServiceException.class)
+    public ResponseEntity<ErrorResponseModel> genericBookServiceException(BookServiceException bookServiceException){
+        return new ResponseEntity<>(toExceptionModel(bookServiceException),bookServiceException.getHttpStatus());
+    }
+
+
 
 
 
